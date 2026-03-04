04e5a40390eaf66e6de9738dc80f4d2265122397
MarcosVallas <63065135+marcos-vallas@users.noreply.github.com>
2026-03-04 18:22:04 -0300

    Agrego Shaders
    Efecto desvanecimiento de la montania

--- scripts/power_up.gd
+++ scripts/power_up.gd
@@ -2,10 +2,17 @@ extends Area2D
 
 @export var value: int = 1 
 
+var velocidad_y = 0.0
+var gravedad = 900.0
+var en_el_suelo = false
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
+	area_entered.connect(_on_area_entered)
+	
 	
 func _on_body_entered(body: Node) -> void:
+	#print("Body")
 	if body.is_in_group("Player"):
 		Global.add_potions(value)
 		$PowerUp.play()
@@ -13,4 +20,36 @@ func _on_body_entered(body: Node) -> void:
 		$CollisionShape2D.queue_free() 
 		await get_tree().create_timer(2.5).timeout
 		queue_free()
+	#if body.is_in_group("Ground"):
+		#print("body piso")
+		#en_el_suelo = true
+		#velocidad_y = 0
+		#$PowerUp.play()
+	# Si el objeto que tocamos es el suelo (un StaticBody2D)
+	
 		
+		
+		
+func _process(delta):
+	if not en_el_suelo:
+		# Aplicar gravedad
+		velocidad_y += gravedad * delta
+		position.y += velocidad_y * delta
+		
+func _physics_process(delta: float) -> void:
+	if en_el_suelo:
+		velocidad_y = 0
+
+func _on_area_entered(area: Area2D) -> void:
+	#print("Area")
+	if area.is_in_group("Ground"):
+		print("area piso")
+		print(position.y)
+		en_el_suelo = true
+		velocidad_y = 0
+		# Ajustar posición para que no se hunda (opcional)
+	pass # Replace with function body.
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
---
+++ scripts/power_upchart.gd
@@ -0,0 +1,61 @@
+extends CharacterBody2D
+
+@export var value: int = 1 
+
+var gravedad = 980 # Fuerza de atracción
+var velocidad = Vector2.ZERO
+
+var en_el_suelo = false
+var recolectado = false
+
+func _ready() -> void:
+	$Area2D.body_entered.connect(_on_body_entered)
+	$Area2D.area_entered.connect(_on_area_entered)
+
+	pass
+	
+	
+func _on_body_entered(body: Node) -> void:
+	#print("Body")
+	if body.is_in_group("Player"):
+		recolectado = true
+		Global.add_potions(value)
+		$PowerUp.play()
+		$Sprite2D.visible = false
+		$CollisionShape2D.queue_free() 
+		await get_tree().create_timer(2.5).timeout
+		queue_free()
+		
+		
+		
+func _physics_process(delta):
+	# Si NO está tocando el suelo, aplicamos gravedad
+	if not is_on_floor():
+		velocity.y += gravedad * delta
+	else:
+		velocity.y = 0
+		print("en el piso")
+		destruir()
+		
+	# Mueve el objeto y detecta colisiones automáticamente
+	move_and_slide()
+
+func destruir():
+	await get_tree().create_timer(5).timeout
+	if !recolectado:
+		$Sprite2D.visible = false
+		$Area2D.queue_free()
+		$CollisionShape2D.queue_free()
+		queue_free()
+	pass
+	
+	
+func _on_area_entered(area: Area2D) -> void:
+	#print("Area")
+	#if area.is_in_group("Ground"):
+		#print("area piso")
+		#print(position.y)
+		#en_el_suelo = true
+		#velocity.y = 0
+		# Ajustar posición para que no se hunda (opcional)
+	pass # Replace with function body.
